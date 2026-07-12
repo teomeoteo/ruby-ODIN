1ef7914476ea815ec9213658b4426836dfe0af19
teomac <liquidinobusiness@gmail.com>
2026-07-12 15:18:54 +0200

    Initiate spec file for caesar cipher

---
+++ spec/caesar_cipher_repeat_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../repeat_exercises/caesar_cipher_repeat'
+
+RSpec.describe '#caesar_cipher' do
+  it do
+    #
+  end
+  it do
+    #
+  end
+  it do
+    #
+  end
+  it do
+    #
+  end
+  it do
+    #
+  end
+  it do
+    #
+  end
+end
