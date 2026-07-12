b4e35a5ec5adbc7fdcd42d716f15e167a7043f0b
teomac <liquidinobusiness@gmail.com>
2026-07-12 15:28:15 +0200

    Add 3 simple tests to the caesar spec

--- spec/caesar_cipher_repeat_spec.rb
+++ spec/caesar_cipher_repeat_spec.rb
@@ -1,14 +1,14 @@
 require_relative '../repeat_exercises/caesar_cipher_repeat'
 
 RSpec.describe '#caesar_cipher' do
-  it do
-    #
+  it 'shifts a char to the right by 1' do
+    expect(caesar_cipher('a', 1)).to eq('b')
   end
-  it do
-    #
+  it 'shifts 3 characters to the right by 2' do
+    expect(caesar_cipher('cde', 2)).to eq('efg')
   end
-  it do
-    #
+  it 'does not change invalid characters' do
+    expect(caesar_cipher('13e', 2)).to eq('13g')
   end
   it do
     #
